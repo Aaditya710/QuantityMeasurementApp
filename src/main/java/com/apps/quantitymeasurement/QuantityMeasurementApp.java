57d96250851e64be8f5053c5da36dc6bd56a6afb
Aaditya710 <aadityajoshi9826@gmail.com>
2026-02-23 14:39:02 +0530

    [Aaditya] : Implemented UC1 Feet measurement equality with immutable Feet class and equals() override

---
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,10 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,10 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementAppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
