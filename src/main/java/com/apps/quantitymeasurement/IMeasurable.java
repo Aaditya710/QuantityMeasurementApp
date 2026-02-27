0ac1f216c56d0986601481045826f27cd5663084
Aaditya710 <aadityajoshi9826@gmail.com>
2026-02-27 14:53:33 +0530

    [Aaditya] : Completed UC11 - VolumeMeasurement

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -2,11 +2,11 @@ package com.apps.quantitymeasurement;
 
 public interface IMeasurable {
 
-	double getConversionFactor();
+    double getConversionFactor();
 
-	double convertToBaseUnit(double value);
+    double convertToBaseUnit(double value);
 
-	double convertFromBaseUnit(double baseValue);
+    double convertFromBaseUnit(double baseValue);
 
-	String getUnitName();
+    String getUnitName();
 }
